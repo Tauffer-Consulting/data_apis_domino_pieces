8b9202c63d471b6a690b944a074792ab909eaaca
luiz <luiz_tauffer@hotmail.com>
2023-11-08 12:26:30 +0100

    tests

--- pieces/NewsApiHeadlinesPiece/test_newsapiheadlines.py
+++ pieces/NewsApiHeadlinesPiece/test_newsapiheadlines.py
@@ -1,21 +1,17 @@
 from domino.testing import piece_dry_run
-from datetime import date, timedelta
 import os
 
 
-def test_newsapi_articles():
-    to_date = date.today().isoformat()
-    from_date = (date.today() - timedelta(days=10)).isoformat()
+def test_newsapi_headlines():
     input_data = dict(
-        query="economy",
-        from_date=from_date,
-        to_date=to_date,
-        sort_by="relevancy",
+        query="dollar",
+        category="business",
+        country="all",
         language="en",
         number_of_results=10
     )
     piece_output = piece_dry_run(
-        piece_name="NewsApiArticlesPiece",
+        piece_name="NewsApiHeadlinesPiece",
         input_data=input_data,
         secrets_data=dict(
             NEWSAPI_API_KEY=os.environ["NEWSAPI_API_KEY"]
@@ -28,6 +24,4 @@ def test_newsapi_articles():
     for article in piece_output["articles"]:
         assert 'title' in article
         assert 'author' in article
-        assert 'publishedAt' in article
-        assert 'url' in article
-        assert 'content' in article
+        assert 'source' in article
