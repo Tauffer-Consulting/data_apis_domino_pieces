26dd43329b4853146a01a5dd7b3bbe651af6eb6b
luiz <luiz_tauffer@hotmail.com>
2023-11-14 20:17:05 +0100

    repo name, tests

--- pieces/NewsApiArticlesPiece/test_newsapiarticle.py
+++ pieces/NewsApiArticlesPiece/test_newsapiarticle.py
@@ -4,30 +4,31 @@ import os
 
 
 def test_newsapi_articles():
-    to_date = date.today().isoformat()
-    from_date = (date.today() - timedelta(days=10)).isoformat()
-    input_data = dict(
-        query="economy",
-        from_date=from_date,
-        to_date=to_date,
-        sort_by="relevancy",
-        language="en",
-        number_of_results=10
-    )
-    piece_output = piece_dry_run(
-        piece_name="NewsApiArticlesPiece",
-        input_data=input_data,
-        secrets_data=dict(
-            NEWSAPI_API_KEY=os.environ["NEWSAPI_API_KEY"]
-        ),
-    )
+    if os.environ.get("NEWSAPI_API_KEY", None):
+        to_date = date.today().isoformat()
+        from_date = (date.today() - timedelta(days=10)).isoformat()
+        input_data = dict(
+            query="economy",
+            from_date=from_date,
+            to_date=to_date,
+            sort_by="relevancy",
+            language="en",
+            number_of_results=10
+        )
+        piece_output = piece_dry_run(
+            piece_name="NewsApiArticlesPiece",
+            input_data=input_data,
+            secrets_data=dict(
+                NEWSAPI_API_KEY=os.environ["NEWSAPI_API_KEY"]
+            ),
+        )
 
-    assert 'message' in piece_output
-    assert 'articles' in piece_output
-    assert len(piece_output["articles"]) <= 10
-    for article in piece_output["articles"]:
-        assert 'title' in article
-        assert 'author' in article
-        assert 'publishedAt' in article
-        assert 'url' in article
-        assert 'content' in article
+        assert 'message' in piece_output
+        assert 'articles' in piece_output
+        assert len(piece_output["articles"]) <= 10
+        for article in piece_output["articles"]:
+            assert 'title' in article
+            assert 'author' in article
+            assert 'publishedAt' in article
+            assert 'url' in article
+            assert 'content' in article
