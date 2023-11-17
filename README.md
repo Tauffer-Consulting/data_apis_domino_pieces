7e67f55896d4293a6bfdb7e10a79b4ad18a1ac46
luiz <luiz_tauffer@hotmail.com>
2023-11-17 11:42:53 +0100

    dogfactspiece

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Domino Pieces for a variety of Data APIs:
 - **NewsApiHeadlinesPiece** - Retrieves headlines using [News API](https://newsapi.org/)
 - **NasaEarthImagePiece** - Retrieves images of Earth using [NASA's EPIC API](https://epic.gsfc.nasa.gov/about/api)
 - **AlphaVantageDailyPiece** - Retrieves daily stock data using [Alpha Vantage API](https://www.alphavantage.co)
+- **DogFactsPiece** - Retrieves random dog facts using the [Dog API](https://dog-api.kinduff.com/api/facts)
---
+++ pieces/DogFactsPiece/piece.py
@@ -0,0 +1,37 @@
+from domino.base_piece import BasePiece
+from .models import InputModel, OutputModel
+import requests
+
+
+class DogFactsPiece(BasePiece):
+    """
+    This Piece uses the Dog API to get a random dog fact.
+    - https://dog-api.kinduff.com/api/facts
+    """
+    def piece_function(self, input_data: InputModel):
+
+        endpoint = 'https://dog-api.kinduff.com/api/facts'
+        try:
+            response = requests.get(endpoint)
+            if response.status_code == 200:
+                data = response.json()
+                dog_fact = data['facts'][0]
+            else:
+                raise Exception(f'Error: {response.status_code} - {response.text}')
+        except Exception as e:
+            raise Exception(f'Error: {e}')
+
+        # Display to results
+        md_text = f"""## Dog Fact
+{dog_fact}\n
+"""
+        with open(f"{self.results_path}/dog_fact.md", "w") as f:
+            f.write(md_text)
+        self.display_result = {
+            "file_type": "md",
+            "file_path": f"{self.results_path}/dog_fact.md",
+        }
+
+        return OutputModel(
+            dog_fact=dog_fact
+        )
